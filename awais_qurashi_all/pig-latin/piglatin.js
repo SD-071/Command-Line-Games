6ddf19e3f5b044bf1bb5a65818130ba686a61b32
Awais <awaishanifqurashi@gmail.com>
2026-01-14 17:31:50 +0100

    made changed to  the pig latin code

--- awais_qurashi_all/pig-latin/piglatin.js
+++ awais_qurashi_all/pig-latin/piglatin.js
@@ -10,20 +10,20 @@ let vowels = ["a", "e", "i", "o", "u"];
 for (let i = 0; i < alphabets.length; i++) 
 {
   let word = alphabets[i];
-  let firstLetter = word[0];
-  let secondLetter = word[1];
+  let firstLetter = word[0].toLowerCase();
+  let secondLetter = word[1].toLowerCase();
 
   // If a word starts with a vowel add the word “way” at the end of the word.
   if (vowels.includes(firstLetter)) 
-    {result = word + "way ";}
+    {result = result + word + "way ";}
 
   // If a word starts with two consonants move the two consonants to the end of the word and add “ay.”
   else if (!vowels.includes(firstLetter) && !vowels.includes(secondLetter)) 
-    {result = word.slice(2) + word[0] + word[1] + "ay ";}
+    {result = result + word.slice(2) + word[0] + word[1] + "ay ";}
 
   // If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add “ay.”
-  else {result = word.slice(1) + word[0] + "ay ";}
+  else {result = result + word.slice(1) + word[0] + "ay ";}
 }
 
 // Output result
-console.log(result);
+console.log(result.trim());
