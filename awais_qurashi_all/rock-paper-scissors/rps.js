572b3b7bd760999db4e656713bd39aeedb03ef37
Awais <awaishanifqurashi@gmail.com>
2026-01-19 16:56:23 +0100

    made changes recommended by maria after presentation

--- awais_qurashi_all/rock-paper-scissors/rps.js
+++ awais_qurashi_all/rock-paper-scissors/rps.js
@@ -6,18 +6,22 @@ const computer = moves[randomIndex];
 
 let result = "";
 
+if (player != 'rock' && player != 'paper' && player !== 'scissors') 
+   {console. log("Invalid choice. Please choose rock, paper, or scissors.");
+    process.exit();}
+
 if (player === computer) 
   {result = "It's a draw!";} 
   
-  else if (player === "rock" && computer === "scissors") 
-  {result = "You win!";} 
-  
-  else if (player === "paper" && computer === "rock") 
-  {result = "You win!";} 
-  
-  else if (player === "scissors" && computer === "paper") 
-  {result = "You win!";}
+  else if 
+          (
+    (player === "rock" && computer === "scissors") || 
+    (player === "paper" && computer === "rock") || 
+    (player === "scissors" && computer === "paper")
+          )
 
+  {result = "You win!";} 
+  
   else {result = "You lose!";}
 
 console.log("You chose " + player + ". Computer chose " + computer + ". " + result);
